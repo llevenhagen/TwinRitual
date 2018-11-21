51578a7db316b3878418a72726ca642e0cbb5626
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-11-21 15:33:27 -0800

    created server directory and added app.js

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const { sequelize } = require('./models')
+const config = require('./config/config')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+require('./routes')(app)
+
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`server started on port ${config.port}`)
+  })
